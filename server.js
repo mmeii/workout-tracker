ca7c361783be25f2df32d3e9a98919cdaf56c2ab
Mengmei Tu <redmmeii@gmail.com>
2021-02-19 19:38:17 -0600

    add html-routes, express router

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+// const db = require("./models");
 
+// express middleware
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+// add routes
+app.use(require("./routes/html-routes.js"));
+// app.use(require("./routes/api-routes.js"));
 
-
+// connect to db
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+}
+);
 
 // Start the server
 app.listen(PORT, () => {
