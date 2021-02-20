6d5cab78c0bb27b25deb5ccc407699c9e41fe763
Mengmei Tu <redmmeii@gmail.com>
2021-02-19 18:46:00 -0600

    add index.js for models

--- server.js
+++ server.js
@@ -16,3 +16,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
