e5e8fd6bfa7c66da2717fb9c610e7e02f9c96e3a
Mengmei Tu <redmmeii@gmail.com>
2021-02-20 16:21:28 -0600

    update workout model to add totalDuration

--- models/Workout.js
+++ models/Workout.js
@@ -38,7 +38,11 @@ const WorkoutSchema = new Schema({
                 trim: true,
             },
         }
-    ]
+    ],
+    totalDuration: {
+        type: Number,
+        default: 0,
+    }
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
